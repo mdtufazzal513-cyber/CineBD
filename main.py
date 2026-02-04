4ade28cc2a5ddba191e84ed9e5adaf05a2cdd9bd
mdtufazzal513-cyber <mdtufazzal513@gmail.com>
2026-02-04 22:11:45 +0600

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("8490429007:AAEI08HnfXrgbnWUKA8VruiIOohfk8aZm6g")
-print("Bot token from env:", BOT_TOKEN)
-
-if not BOT_TOKEN:
-    print("Error: BOT_TOKEN not found in environment variables!")
-    exit(1)
